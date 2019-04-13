9d0137a98d7b68c06917e989332291ece6639fd9
Max Webb <webbwrites@gmail.com>
2019-04-13 10:59:50 -0600

    logo and img events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+let funLogo = document.querySelector('.logo-heading');
+funLogo.addEventListener('click', function() {
+  window.alert('You clicked on the logo!');
+});
+
+let venice1 = document.querySelector('#funImg');
+venice1.addEventListener('mouseenter', function(event) {
+  event.target.src = 'img/venice.jpg';
+});
+venice1.addEventListener('mouseleave', function(event) {
+  event.target.src = 'img/fun.jpg';
+});
